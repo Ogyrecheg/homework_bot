4526c0475f024f6a568d219f873b61e9eebc69da
Шевченко Александр <ogyrecheg@yandex.ru>
2022-11-22 23:03:15 +0300

    first commit

--- homework.py
+++ homework.py
@@ -1,11 +1,20 @@
-...
+import os
+import logging
+import time
+
+import requests
+
+import telegram
+
+from dotenv import load_dotenv
+from http import HTTPStatus
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGA_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -27,7 +36,14 @@ def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
-    ...
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    response_content = response.json()
+
+    if response.status_code == HTTPStatus.OK:
+        return response_content
+    else:
+        raise
+            logging.error(f'Ошибка при запросе к API: {error}')
 
 
 def check_response(response):
@@ -49,7 +65,19 @@ def parse_status(homework):
 
 
 def check_tokens():
-    ...
+    """Проверяем доступность переменных окружения."""
+
+    tokens = {
+        'practicum_token': PRACTICUM_TOKEN,
+        'telegram_token': TELEGRAM_TOKEN,
+        'telegram_chat_id': TELEGRAM_CHAT_ID,
+    }
+
+    for key, value in tokens.items():
+        if value is None:
+            return False
+
+    return True
 
 
 def main():
